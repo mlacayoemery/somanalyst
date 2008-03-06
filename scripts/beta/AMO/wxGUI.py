a29dd385883f6e691d47d23e40d1774c6e2f3da6
mlacayoemery <mlacayoemery@gmail.com>
2008-03-06 17:03:50 +0000

    Began AM gui.

---
+++ scripts/beta/AMO/wxGUI.py
@@ -0,0 +1,100 @@
+import wx
+
+def OnTaskBarRight(event):
+    app.ExitMainLoop()
+
+class MyApp(wx.App):
+    
+    def OnInit(self):
+       frame = MyFrame("Abstract Map - Manager", (0, 0), (800,600))
+       frame.Show()
+       self.SetTopWindow(frame)
+       #frame.Maximize(True)
+       #frame.SetIcon(wx.Icon("icon.bmp", wx.BITMAP_TYPE_ICO))
+       return True
+    
+class MyFrame(wx.Frame):
+    
+    def __init__(self, title, pos, size):
+        wx.Frame.__init__(self, None, -1, title, pos, size)
+        panel = wx.Panel(self, -1)
+
+        wx.StaticText(panel, -1, "Collections",(0, 0))
+
+        sampleList = ['collection1','collection2','collection3']
+
+        listBox = wx.ListBox(panel, -1, (0, 15), (150, 100), sampleList, 
+                wx.LB_SINGLE)
+        listBox.SetSelection(0)
+
+        self.button = wx.Button(panel, -1, "Export", pos=(152, 41))
+        self.Bind(wx.EVT_BUTTON, self.OnClick, self.button)
+        self.button.SetDefault()
+
+        self.button = wx.Button(panel, -1, "Rename", pos=(152, 64))
+        self.Bind(wx.EVT_BUTTON, self.OnClick, self.button)
+        self.button.SetDefault()
+
+        self.button = wx.Button(panel, -1, "Delete", pos=(152, 87))
+        self.Bind(wx.EVT_BUTTON, self.OnClick, self.button)
+        self.button.SetDefault()
+
+        self.button = wx.Button(panel, -1, "Import", pos=(152, 17))
+        self.Bind(wx.EVT_BUTTON, self.OnClick, self.button)
+        self.button.SetDefault()
+
+        wx.StaticText(panel, -1, "Completed Projects",(233, 0))        
+
+        sampleList2 = ['project1','project2','project3']
+        listBox = wx.ListBox(panel, -1, (233, 15), (200, 100), sampleList2, 
+                wx.LB_SINGLE)
+        listBox.SetSelection(0)
+
+        self.button = wx.Button(panel, -1, "Copy", pos=(435, 17))
+
+        self.button = wx.Button(panel, -1, "Rename", pos=(435, 41))
+
+        self.button = wx.Button(panel, -1, "Delete", pos=(435, 64))
+
+        wx.StaticText(panel, -1, "Projects in Progress (",(0, 270))        
+
+        sampleList2 = ['project1','project2','project3']
+        listBox = wx.ListBox(panel, -1, (0, 285), (200, 100), sampleList2, 
+                wx.LB_SINGLE)
+        listBox.SetSelection(0)
+
+        self.button = wx.Button(panel, -1, "New", pos=(202, 287))
+
+        self.button = wx.Button(panel, -1, "Rename", pos=(202, 311))
+
+        self.button = wx.Button(panel, -1, "Delete", pos=(202, 334))
+        wx.CheckBox(panel, -1, "Include Completed)", (102, 268), (150, 18))
+
+        wx.StaticText(panel, -1, "Base Index",(0, 387))    
+
+
+    def OnClick(self, event):
+        self.button.SetLabel("Clicked")        
+        
+    def OnQuit(self, event):
+        self.Close()
+         
+    def OnAbout(self, event):
+        wx.MessageBox("Copyright 2008 Martin Lacayo\n\nThanks to my thesis committee:\nAndr\xe9 Skupin, Serge Rey, Carl Eckberg\n\n"+
+                      "For futher information visit:\nhttp://somanalyst.hopto.org","About SOM Analyst",
+                      wx.OK | wx.ICON_INFORMATION, self)
+                
+if __name__ == '__main__':
+    app = MyApp(False)
+
+##    #setup icon object
+##    
+##
+##    #setup taskbar icon
+##    tbicon = wx.TaskBarIcon()
+##    tbicon.SetIcon(icon, "I am an Icon")
+##    #add taskbar icon event
+##    wx.EVT_TASKBAR_RIGHT_UP(tbicon, OnTaskBarRight)
+
+    app.MainLoop()
+
