23caf06e199113574754f7c5918ba39018799b25
mlacayoemery <mlacayoemery@gmail.com>
2009-05-05 10:03:25 +0000

    Modified SOM data labeling to preserve order.

--- lib/som/DBFtoDAT.py
+++ lib/som/DBFtoDAT.py
@@ -12,9 +12,11 @@ def DBFtoDAT(inName,labels,outName):
     outFile.write(str(len(d.fieldnames)-len(labels)))
 
     labelHeaders=map(d.fieldnames.pop,indicies)
+    labelHeaders.reverse()
     outFile.write("\n#n "+' '.join(d.fieldnames+labelHeaders))
 
     for row in d:
-        rowLabels=map(row.pop,indicies)
+        rowLabels=[label.strip().replace(" ","_") for label in map(row.pop,indicies)]
+        rowLabels.reverse()
         outFile.write("\n"+' '.join(row+rowLabels))
     outFile.close()
