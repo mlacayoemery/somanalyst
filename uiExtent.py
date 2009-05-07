eb10db8e22f3e604502d1417fb280675f074cf6b
mlacayoemery <mlacayoemery@gmail.com>
2009-05-07 21:15:36 +0000

    Updated extent to new architecture.

--- uiExtent.py
+++ uiExtent.py
@@ -2,6 +2,6 @@ import sys
 import lib.som.extent
 
 if __name__=="__main__":
-    ifile=sys.argv[1]
-    ofile=sys.argv[2].split('.')[0]
+    ifile=sys.argv[1].split('.')[0]
+    ofile=sys.argv[3].split('.')[0]
     lib.som.extent.extent(ifile,ofile)
