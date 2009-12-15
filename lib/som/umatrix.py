4d19b90c5ccbd87ffbdfcadcaa6fa2e3b3d84597
mlacayoemery <mlacayoemery@gmail.com>
2009-12-15 07:31:43 +0000

    Corrected calculation error.

--- lib/som/umatrix.py
+++ lib/som/umatrix.py
@@ -12,35 +12,47 @@ def uMatrix(inName,outName):
             number=0
             #starting at noon, going clockwise
             try:
-               total+=SOMclass.qError(centroid,som.vectors[i+1][j])
-               number+=1
+                if i == som.ydimension - 1:
+                    raise IndexError
+                total+=SOMclass.qError(centroid,som.vectors[i+1][j])
+                number+=1
             except IndexError:
                 pass
             try:
-               total+=SOMclass.qError(centroid,som.vectors[i][j+1])
-               number+=1
+                if j == som.xdimension - 1:
+                    raise IndexError
+                total+=SOMclass.qError(centroid,som.vectors[i][j+1])
+                number+=1
             except IndexError:
                 pass
             try:
-               total+=SOMclass.qError(centroid,som.vectors[i-1][j])
-               number+=1
+                if i == 0:
+                    raise IndexError
+                total+=SOMclass.qError(centroid,som.vectors[i-1][j])
+                number+=1
             except IndexError:
                 pass
             try:
-               total+=SOMclass.qError(centroid,som.vectors[i-1][j-1])
-               number+=1
+                if i == 0 or j == 0:
+                    raise IndexError
+                total+=SOMclass.qError(centroid,som.vectors[i-1][j-1])
+                number+=1
             except IndexError:
                 pass
             try:
-               total+=SOMclass.qError(centroid,som.vectors[i][j-1])
-               number+=1
+                if j == 0:
+                    raise IndexError
+                total+=SOMclass.qError(centroid,som.vectors[i][j-1])
+                number+=1
             except IndexError:
                 pass
             try:
-               total+=SOMclass.qError(centroid,som.vectors[i+1][j-1])
-               number+=1
+                if i == som.ydimension - 1 or j == 0:
+                    raise IndexError
+                total+=SOMclass.qError(centroid,som.vectors[i+1][j-1])
+                number+=1
             except IndexError:
                 pass
             value=str(round(total/number,6))
-            dbf.addRow([value[:values.rfind(".")+6]])
+            dbf.addRow([value[:value.rfind(".")+6]])
     dbf.writeFile(outName)
