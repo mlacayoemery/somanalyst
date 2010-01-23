8fa2f4428362178a28cfb052cf4b32af51790b0c
mlacayoemery <mlacayoemery@gmail.com>
2010-01-23 01:02:06 +0000

    Added context sensitive help.

--- doc/make.bat
+++ doc/make.bat
@@ -1,2 +1,3 @@
 C:\Python25\Scripts\sphinx-build.exe -b htmlhelp -a sphinx html > log.txt
+type sphinx\context.txt >> html\SOMAnalyst.hhp
 "C:\Program Files\HTML Help Workshop\hhc.exe" html\SOMAnalyst.hhp >> log.txt
